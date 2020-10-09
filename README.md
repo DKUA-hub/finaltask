20e6759fa867b14bee6da170165d8f1047ab5ebb
Dmitriy <dmytro.kolychev@gmail.com>
2020-10-10 01:00:11 +0300

    Initial setup of working folder. Created template files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# stepic_course
+
+Link to the course
+https://stepik.org/course/575/syllabus
+
+########################################
+Final task from Stepic' Auto testing course
---
+++ conftest.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action = 'store', default='en',
+                     help = 'Choose language: es, ru, en')
+
+@pytest.fixture(scope='function')
+def browser(request):
+    print("\nStarting fixture")
+    lang = request.config.getoption('language')
+    print(f"\nUser's language is {lang}")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nBye..")
+    browser.quit()
