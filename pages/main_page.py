1815fe74f9e91c4c19eb088c397beccd2db9eea1
Dmitriy <dmytro.kolychev@gmail.com>
2020-10-28 00:27:34 +0200

    Added tests for register and login forms

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,14 @@
 from .base_page import BasePage
+from .locators import MainLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
+#import selenium.webdriver.remote.webdriver
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainLocators.LOGIN_LINK)
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainLocators.LOGIN_LINK), "No login link"
